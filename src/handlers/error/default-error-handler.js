1c46b55d276260db9109fe85c0430722d7b7c142
connellrobert <robert.lel.connell@gmail.com>
2021-08-24 08:49:07 -0500

    closer to getting 400 level status codes

--- src/handlers/error/default-error-handler.js
+++ src/handlers/error/default-error-handler.js
@@ -1,11 +1,12 @@
 
 
 
-exports.handler = async (event, context) => {
+exports.handler = (event, context, callback) => {
     const {result} = event;
-    console.error(`State machine ended with result: ${result}`);
-    return {
-        statusCode: result.status,
-        reason: result.reason
-    }
+    console.error(`State machine ended with result: ${JSON.stringify(result)}`);
+    // return {
+    //     statusCode: result.status,
+    //     reason: result.reason
+    // }
+    callback(result.reason, result.status)
 }
